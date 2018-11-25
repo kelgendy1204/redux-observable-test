5c66118d2a3bcf697085e8f654047c52e34c5723
Khaled Saad <elgendy1204@gmail.com>
2018-11-25 11:47:57 +0200

    Fix linting rules

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,18 +1,18 @@
-import { connect } from 'react-redux'
-import { setVisibilityFilter } from '../actions'
-import Link from '../components/Link'
+import { connect } from 'react-redux';
+import { setVisibilityFilter } from '../actions';
+import Link from '../components/Link';
 
 const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-})
+    active: ownProps.filter === state.visibilityFilter
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  setFilter: () => {
-    dispatch(setVisibilityFilter(ownProps.filter))
-  }
-})
+    setFilter: () => {
+        dispatch(setVisibilityFilter(ownProps.filter));
+    }
+});
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link)
+    mapStateToProps,
+    mapDispatchToProps
+)(Link);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
-import 'todomvc-app-css/index.css'
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import App from './components/App';
+import reducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import 'todomvc-app-css/index.css';
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger)));
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
